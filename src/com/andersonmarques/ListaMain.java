b6504f4f8f32d2fb6be26c16a80bccd9a20a2373
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-03-14 15:21:22 -0300

    Usando Vector para lista thread safe

--- src/com/andersonmarques/ListaMain.java
+++ src/com/andersonmarques/ListaMain.java
@@ -1,21 +1,33 @@
 package com.andersonmarques;
 
-import com.andersonmarques.model.Lista;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
 import com.andersonmarques.model.TarefaAdicionarElemento;
 
 public class ListaMain {
 
 	public static void main(String[] args) throws InterruptedException {
-		Lista lista = new Lista();
+		
+		/*Não é thread safe*/
+		List<String> listaNaoThreadSafe = new ArrayList<>();
+		
+		/*List por padrão não é sincronizada, podemos mudar este estado com o Collections.synchronizedList*/
+		List<String> listaThreadSafeComCollections = Collections.synchronizedList(new ArrayList<>());
+		
+		/*ou*/
+		Vector<String> listaThreadSafePadrao = new Vector<>();
 		
 		for (int i = 0; i < 10; i++) {
-			new Thread(new TarefaAdicionarElemento(lista, i)).start();
+			new Thread(new TarefaAdicionarElemento(listaThreadSafePadrao, i)).start();
 		}
 		
 		Thread.sleep(1000);
 		
-		for (int i = 0; i < lista.tamanho(); i++) {
-			System.out.println(i+" - "+lista.pegarElementoNaPosicao(i));
+		for (int i = 0; i < listaThreadSafePadrao.size(); i++) {
+			System.out.println(i+" - "+listaThreadSafePadrao.get(i));
 		}
 	}
 }
--- src/com/andersonmarques/model/TarefaAdicionarElemento.java
+++ src/com/andersonmarques/model/TarefaAdicionarElemento.java
@@ -1,11 +1,13 @@
 package com.andersonmarques.model;
 
+import java.util.List;
+
 public class TarefaAdicionarElemento implements Runnable {
 
-	private Lista lista;
 	private int numeroThread;
+	private List<String> lista;
 
-	public TarefaAdicionarElemento(Lista lista, int numeroThread) {
+	public TarefaAdicionarElemento(List<String> lista, int numeroThread) {
 		this.lista = lista;
 		this.numeroThread = numeroThread;
 	}
@@ -13,8 +15,7 @@ public class TarefaAdicionarElemento implements Runnable {
 	@Override
 	public void run() {
 		for (int i = 0; i < 100; i++) {
-			lista.addElemento(String.format("Thread %d - elemento %d", numeroThread, i));
+			lista.add(String.format("Thread %d - elemento %d", numeroThread, i));
 		}
 	}
-
 }
