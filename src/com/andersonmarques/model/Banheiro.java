148f6d21ee4d9423206d98584ed7b3fdb532b086
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-03-14 11:00:01 -0300

    Usando ReentrantLock no lugar do Synchronized

--- src/com/andersonmarques/model/Banheiro.java
+++ src/com/andersonmarques/model/Banheiro.java
@@ -1,32 +1,38 @@
 package com.andersonmarques.model;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Banheiro {
+	
+	private ReentrantLock lock = new ReentrantLock();
 
 	public void fazerNumero1() {
 		String nome = Thread.currentThread().getName();
 		System.out.println(nome + " bateu na porta");
-
-		/**
-		 * synchronized -> Apenas um thread por vez, sua execução ocorre de forma
-		 * atômica, ela não pode ser interrompida na metade.
-		 */
-		synchronized (this) {
-			usarBanheiro(nome, 5000);
-		}
+		
+		/** lock trava o acesso de outras threads */
+		lock.lock();
+		
+		usarBanheiro(nome, 5000, "rápida");
+		
+		/** libera o acesso. */
+		lock.unlock();
 	}
 
 	public void fazerNumero2() {
 		String nome = Thread.currentThread().getName();
 		System.out.println(nome + " bateu na porta");
 
-		synchronized (this) {
-			usarBanheiro(nome, 15000);
-		}
+		lock.lock();
+		
+		usarBanheiro(nome, 15000, "demorada");
+
+		lock.unlock();
 	}
 
-	private void usarBanheiro(String nome, long tempoEstimado) {
+	private void usarBanheiro(String nome, long tempoEstimado, String tempoDescrito) {
 		System.out.println(nome + " entrando no banheiro");
-		System.out.println(nome + " fazendo coisa rapida");
+		System.out.println(nome + " fazendo coisa "+tempoDescrito);
 
 		try {
 			Thread.sleep(tempoEstimado);
