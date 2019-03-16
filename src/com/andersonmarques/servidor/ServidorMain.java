b4b9b4081310cf4b076f73a3ede8290b29804111
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-03-15 21:46:35 -0300

    Criado pool de threads
    Criado pool de thread automático e manual

--- src/com/andersonmarques/servidor/ServidorMain.java
+++ src/com/andersonmarques/servidor/ServidorMain.java
@@ -2,6 +2,8 @@ package com.andersonmarques.servidor;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.andersonmarques.servidor.tarefa.DistribuirTarefa;
 
@@ -9,19 +11,26 @@ public class ServidorMain {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("---Subindo Servidor na porta 54321---");
-		
+
 		/**
-		 * Socket - Canal de comunicação entre duas maquinas, 
-		 * ServerSocket (Servidor) e Socket (Cliente).
+		 * Socket - Canal de comunicação entre duas maquinas, ServerSocket (Servidor) e
+		 * Socket (Cliente).
 		 */
 		ServerSocket serverSocket = new ServerSocket(54321);
-		
+
+		/**
+		 * Define o tamanho maximo do pool de threads -> newFixedThreadPool Define o
+		 * tamanho de pool aumentando e diminuindo de forma automática ->
+		 * newCachedThreadPool
+		 */
+		ExecutorService threadPool = Executors.newCachedThreadPool();
+
 		while (true) {
 			Socket socketRequisicaoClient = serverSocket.accept();
 			System.out.println("Cliente recebido na porta: " + socketRequisicaoClient.getPort());
 
 			DistribuirTarefa distribuirTarefa = new DistribuirTarefa(socketRequisicaoClient);
-			new Thread(distribuirTarefa).start();
+			threadPool.execute(distribuirTarefa);
 		}
 	}
 }
