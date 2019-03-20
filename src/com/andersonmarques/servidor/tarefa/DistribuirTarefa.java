04a53cd015fca4d5f2d37f9556e7be73a545e46b
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-03-20 14:30:45 -0300

    Chamando threads com retorno

--- src/com/andersonmarques/servidor/tarefa/DistribuirTarefa.java
+++ src/com/andersonmarques/servidor/tarefa/DistribuirTarefa.java
@@ -1,14 +1,15 @@
 package com.andersonmarques.servidor.tarefa;
 
-import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 import com.andersonmarques.servidor.service.ServidorService;
 import com.andersonmarques.servidor.tarefa.comando.ComandoC1;
-import com.andersonmarques.servidor.tarefa.comando.ComandoC2;
+import com.andersonmarques.servidor.tarefa.comando.ComandoC2AcessaBanco;
+import com.andersonmarques.servidor.tarefa.comando.ComandoC2ChamaWebService;
 
 public class DistribuirTarefa implements Runnable {
 
@@ -16,7 +17,8 @@ public class DistribuirTarefa implements Runnable {
 	private ServidorService servidorService;
 	private ExecutorService threadPool;
 
-	public DistribuirTarefa(ExecutorService threadPool, Socket socketRequisicaoClient, ServidorService servidorService) {
+	public DistribuirTarefa(ExecutorService threadPool, Socket socketRequisicaoClient,
+			ServidorService servidorService) {
 		this.threadPool = threadPool;
 		this.socketRequisicaoClient = socketRequisicaoClient;
 		this.servidorService = servidorService;
@@ -39,30 +41,34 @@ public class DistribuirTarefa implements Runnable {
 				executarComando(scanner.nextLine(), printStream);
 			}
 			scanner.close();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void executarComando(String comando, PrintStream printStream) throws IOException {
+	private void executarComando(String comando, PrintStream printStream) throws Exception {
 		System.out.println("Recebeu " + comando);
 
 		switch (comando.toLowerCase()) {
-			case "c1":
-				printStream.println("Confirmação comando c1");
-				threadPool.execute(new ComandoC1(printStream));
-				break;
-			case "c2":
-				printStream.println("Confirmação comando c2");
-				threadPool.execute(new ComandoC2(printStream));
-				break;
-			case "fim":
-				printStream.println("Desligando servidor");
-				servidorService.desligar();
-				break;
-			default:
-				printStream.println("Comando inválido.");
-				break;
+		case "c1":
+			printStream.println("Confirmação comando c1");
+			threadPool.execute(new ComandoC1(printStream));
+			break;
+		case "c2":
+			printStream.println("Confirmação comando c2");
+			
+			/* submit equivalente ao execute para Runnable */
+			Future<String> respostaWS = threadPool.submit(new ComandoC2ChamaWebService());
+			Future<String> respostaDB = threadPool.submit(new ComandoC2AcessaBanco());
+			threadPool.execute(new TrataResultadosFuture(respostaWS, respostaDB, printStream));
+			break;
+		case "fim":
+			printStream.println("Desligando servidor");
+			servidorService.desligar();
+			break;
+		default:
+			printStream.println("Comando inválido.");
+			break;
 		}
 	}
 }
